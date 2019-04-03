750276a5cd355384881c64382a3e4e9851f5d40a
JiangXJ1024 <17863110507@163.com>
2019-04-03 20:03:13 +0800

    服务器端+客户端2

---
+++ MyCome0/MyCome0/app/src/main/java/com/example/mycome0/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.mycome0;
+
+import android.app.Application;
+
+public class MyApplication extends android.app.Application {
+
+    User user;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        user = new User();
+    }
+}
