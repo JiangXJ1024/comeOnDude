22a20404862754e165538e8c0cd5be4e2ae4b09c
shiningMMQ <724455347@qq.com>
2019-04-03 18:22:54 +0800

    Revert "12"
    This reverts commit 1f40bec75fb750887c882c10f8965ceb7143ec25.

---
+++ MyCome0/MyCome0/app/src/main/java/com/example/mycome0/Message2Activity.java
@@ -0,0 +1,23 @@
+package com.example.mycome0;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Message2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message2);
+    }
+
+    public void Click7(View view) {//查看动态--跳转至动态页面2
+
+    }
+
+    public void Click8(View view) {//修改信息--跳转至两人私聊界面
+
+    }
+}
